f2d7ee2080e6d9938248eee60afa358e7ce45c17
Blanchy <blanchypolangcos@gmail.com>
2016-12-09 21:59:56 -0800

    added isAvailable(startDate, endDate)

--- src/Room.java
+++ src/Room.java
@@ -43,6 +43,36 @@ public class Room {
         return flag;
     }
 
+    /**
+     * Checks dates for availability. Assumes dates are in format MM/DD/YYYY
+     * @param date1 the start date in MM/DD/YYYY
+     * @param date2 the end date in MM/DD/YYYY
+     * @return if room is available between these dates
+     */
+    public boolean isAvailable(String date1, String date2) {
+    	String[] dateArray1 = date1.split("/");
+    	String[] dateArray2 = date2.split("/");
+    	
+    	Calendar start = new GregorianCalendar(Integer.parseInt(dateArray1[1]), 
+    			Integer.parseInt(dateArray1[0]) + 1, Integer.parseInt(dateArray1[2]));
+    	start.clear(Calendar.HOUR); start.clear(Calendar.MINUTE); start.clear(Calendar.SECOND);start.clear(Calendar.MILLISECOND);
+    	
+    	Calendar end = new GregorianCalendar(Integer.parseInt(dateArray2[1]), 
+    			Integer.parseInt(dateArray2[0]) + 1, Integer.parseInt(dateArray2[2]));
+    	end.clear(Calendar.HOUR); end.clear(Calendar.MINUTE); end.clear(Calendar.SECOND);end.clear(Calendar.MILLISECOND);
+    	
+    	while (!start.equals(end)) {
+    		String testDate = start.get(Calendar.MONTH) + "/" + 
+    				start.get(Calendar.DATE) + "/" + start.get(Calendar.YEAR);
+    		if (!isAvailable(testDate)) { 			
+    			return false;
+    		}
+    		start.add(Calendar.DATE, 1);
+    	}
+    	
+    	return true;
+    }
+    
     public ArrayList<Reservation> getAllReservations()
     {
         return reservations;
