3e6eefbf25d5a3f0a6100e548f1dcceb6424329e
Blanchy <blanchypolangcos@gmail.com>
2016-12-10 16:54:20 -0800

    contains main method

---
+++ src/HotelReservation.java
@@ -0,0 +1,20 @@
+/**
+ * @author BlanchyPolangcos
+ * @author ChristopherNavy
+ * @author JonathanWong
+ *
+ * The main class which runs the hotel reservation system.
+ **/
+
+public class HotelReservation {
+
+	/**
+	 * main method to run hotel reservation system
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Hotel model = new Hotel();
+		HotelView test = new HotelView(model);
+	}
+
+}
